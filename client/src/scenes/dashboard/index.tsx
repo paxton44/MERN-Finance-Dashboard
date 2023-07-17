229040b64a6677b8a74378aad157f07bf2459fa3
Mitchell Flood <mitchflood1@gmail.com>
2023-07-17 11:40:29 -0400

    adding styling to boxes with dashboardbox component

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,5 +1,6 @@
+import DashboardBox from "@/components/DashboardBox";
 import { Box, useMediaQuery } from "@mui/material";
-
+// Look at gridTemplateArea docs to see how layout applies based on using letters. This is better than flexbox because its not just left -> right or top -> bottom layout
 const gridTemplateLargeScreens = `
   "a b c"
   "a b c"
@@ -12,6 +13,7 @@ const gridTemplateLargeScreens = `
   "g h j"
   "g h j"
 `;
+// Look at gridTemplateArea docs to see how layout applies based on using letters. Using 1fr and stacking like below you can make another layout based on screen size using a ternary operator
 const gridTemplateSmallScreens = `
   "a"
   "a"
@@ -47,6 +49,7 @@ const gridTemplateSmallScreens = `
  
 
 const Dashboard = () => {
+  // using boolean (isAboveMediumScreens) with the useMediaQuery hook to make the screen respond at 1200px or not 1200px making the layout stack into one individual column for small (mobile) screens.
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
     return (
       <Box
@@ -55,12 +58,15 @@ const Dashboard = () => {
         display="grid"
         gap="1.5rem"
         sx={
+          // Using ternary operator to use conditional statement to adjust box layout based screen size
           isAboveMediumScreens
+          // Large Screens
             ? {
                 gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
                 gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
                 gridTemplateAreas: gridTemplateLargeScreens,
               }
+          // Small Screens 
             : {
                 gridAutoColumns: "1fr",
                 gridAutoRows: "80px",
@@ -68,16 +74,17 @@ const Dashboard = () => {
               }
         }
       >
-        <Box bgcolor="#fff" gridArea="a"></Box>
-        <Box bgcolor="#fff" gridArea="b"></Box>
-        <Box bgcolor="#fff" gridArea="c"></Box>
-        <Box bgcolor="#fff" gridArea="d"></Box>
-        <Box bgcolor="#fff" gridArea="e"></Box>
-        <Box bgcolor="#fff" gridArea="f"></Box>
-        <Box bgcolor="#fff" gridArea="g"></Box>
-        <Box bgcolor="#fff" gridArea="h"></Box>
-        <Box bgcolor="#fff" gridArea="i"></Box>
-        <Box bgcolor="#fff" gridArea="j"></Box>
+        {/* Boxes that are each chart for dashboard using DashboardBox a component i created to handle the styling of each box in a more organized way */}
+        <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="d"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="e"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="f"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="g"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="h"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="i"></DashboardBox>
+        <DashboardBox bgcolor="#fff" gridArea="j"></DashboardBox> 
       </Box>
     );
   };
