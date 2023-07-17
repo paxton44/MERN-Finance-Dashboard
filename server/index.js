f590c8f657f0b86df219669471c4e0609612912d
Mitchell Flood <mitchflood1@gmail.com>
2023-07-17 14:21:52 -0400

    connected mongodb and started backed build

--- server/index.js
+++ server/index.js
@@ -21,3 +21,16 @@ app.use(cors());
 console.log("Hello");
 
 // Mongoose Setup
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+
+    .catch((error) => console.log(`${error} did not connect`));
+      
